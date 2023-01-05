e384c99a683f5c162c5701bd58666a86259f9e8e
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-01-05 22:57:51 +0200

    spinner added

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
-import { getContacts } from 'redux/contacts/contacts-selectors';
-import { getFilter } from 'redux/filter/filter-selectors';
+import {
+  selectContacts,
+  selectIsLoading,
+} from 'redux/contacts/contacts-selectors';
+import { selectFilter } from 'redux/filter/filter-selectors';
 import { Contact } from 'components/Contact/Contact';
 import { List, ListItem, Text, Span } from './ContactList.styled';
+import { Loader } from 'components/Loader/Loader';
 
 export const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,7 +24,9 @@ export const ContactList = () => {
     name.toLowerCase().includes(filter)
   );
 
-  return contacts.length !== 0 && filteredContacts.length !== 0 ? (
+  return contacts.length === 0 && isLoading ? (
+    <Loader />
+  ) : contacts.length !== 0 && filteredContacts.length !== 0 ? (
     <List>
       {filteredContacts.map(({ name, phone, id }) => (
         <ListItem key={id}>
