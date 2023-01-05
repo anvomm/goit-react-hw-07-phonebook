e91d9b06b81e2f93733b50bf2f4ea679a62a1d25
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-01-05 22:57:14 +0200

    notifications added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-/* import { deleteContact } from 'redux/contacts/contactsSlice'; */
 import { deleteContact } from 'redux/operations';
 import { ContactButton } from './Contact.styled';
 
 export const Contact = ({ name, number, id }) => {
   const dispatch = useDispatch();
+
   return (
     <>
       <p>
@@ -14,7 +14,7 @@ export const Contact = ({ name, number, id }) => {
       <ContactButton
         type="button"
         onClick={() => {
-          dispatch(deleteContact(id));
+          dispatch(deleteContact({ id, name }));
         }}
       >
         Delete
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,34 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'https://63b6a5c94f17e3a931baad80.mockapi.io';
+
+const notifyAboutError = () => {
+  toast.error(`Ooops... something went wrong, please try again later.`, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+};
+
+const notifyAboutSuccess = text => {
+  toast(text, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+};
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
@@ -10,18 +37,7 @@ export const fetchContacts = createAsyncThunk(
       const response = await axios.get('/contacts');
       return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async (id, thunkAPI) => {
-    try {
-      const response = await axios.delete(`/contacts/${id}`);
-      return response.data;
-    } catch (error) {
+      notifyAboutError();
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -32,8 +48,26 @@ export const addContact = createAsyncThunk(
   async (contact, thunkAPI) => {
     try {
       const response = await axios.post(`/contacts`, contact);
+      notifyAboutSuccess(
+        `${contact.name} is successfully added to your contacts.`
+      );
       return response.data;
     } catch (error) {
+      notifyAboutError();
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async ({ id, name }, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      notifyAboutSuccess(`${name} is now deleted from your contacts.`);
+      return response.data;
+    } catch (error) {
+      notifyAboutError();
       return thunkAPI.rejectWithValue(error.message);
     }
   }
